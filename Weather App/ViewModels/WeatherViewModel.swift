c7849f459bec87d07d279e4a8ab46a13a2e480f9
Marios Kanellopoulos <mariosknl@outlook.com>
2026-05-07 13:21:34 +0300

    Implement fetchWeatherData using URLSession and Async-Await

--- Weather App/ViewModels/WeatherViewModel.swift
+++ Weather App/ViewModels/WeatherViewModel.swift
@@ -17,17 +17,31 @@ class WeatherViewModel {
     private let apiKey = "1cbb8ba1f21e403eae892004260705"
     
     private func fetchWeatherData(for city: String) async throws -> WeatherResponse {
+        // Build URL
         let urlString = "https://api.weatherapi.com/v1/current.json?key=\(apiKey)&q=\(city)&aqi=no"
+        guard let url = URL(string: urlString) else {
+            throw WeatherError.invalidURL
+        }
         
+        // Fetch data
+        let (data, response) = try await URLSession.shared.data(from: url)
         
-        return .init(
-            location: .init(name: "London", country: "England"),
-            current: .init(
-                tempC: 30.0,
-                condition: .init(text: "Great Conditions", icon: "Icon Name"),
-                feelslikeC: 32.5
-            )
-        )
+        // Validate response
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw WeatherError.unknown
+        }
+        
+        guard httpResponse.statusCode == 200 else {
+            throw WeatherError.requestFailed(statusCode: httpResponse.statusCode)
+        }
+        
+        // Decode Model
+        do {
+            return try JSONDecoder().decode(WeatherResponse.self, from: data)
+        } catch {
+            throw WeatherError.decodingFailed
+        }
+        
     }
 }
 
